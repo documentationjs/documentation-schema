1b0f8beb2caad5a69f162fb8daf07effb2b2bc90
John Firebaugh <john.firebaugh@gmail.com>
2016-03-13 12:03:01 -0700

    Start writing a schema

---
+++ index.js
@@ -0,0 +1,103 @@
+/**
+ * @typedef {Object} Comment
+ * @property {boolean} abstract
+ * @property {('public'|'protected'|'private')} access
+ * @property {Path[]} augments
+ * @property {Author[]} authors
+ * @property {*} borrows
+ * @property {*} callback
+ * @property {Unist} classdesc
+ * @property {*} constructs
+ * @property {Context} context
+ * @property {Unist} copyright
+ * @property {*} default
+ * @property {Unist} deprecated
+ * @property {Unist} description
+ * @property {*} enum
+ * @property {Example[]} examples
+ * @property {*} exports
+ * @property {Path[]} fires
+ * @property {boolean} ignore
+ * @property {Path[]} implements
+ * @property {boolean} interface
+ * @property {('class'|'constant'|'event'|'external'|'file'|'function'|'member'|'mixin'|'module'|'namespace'|'typedef')} kind
+ * @property {*} license
+ * @property {Path[]} listens
+ * @property {Loc} loc
+ * @property {Path} memberof
+ * @property {Members} members
+ * @property {Path[]} mixes
+ * @property {Path} name
+ * @property {boolean} override
+ * @property {Parameter[]} params
+ * @property {Property[]} properties
+ * @property {boolean} readonly
+ * @property {Path[]} requires
+ * @property {Return[]} returns
+ * @property {('global'|'static'|'instance'|'inner')} scope
+ * @property {Unist[]} see
+ * @property {Version} since
+ * @property {Unist} summary
+ * @property {Path} this
+ * @property {Throw[]} throws
+ * @property {Unist[]} todos
+ * @property {*} tutorial
+ * @property {Type} type
+ * @property {*} variation
+ * @property {Version} version
+ */
+
+/**
+ * @typedef {Object} Author
+ */
+
+/**
+ * @typedef {Object} Context
+ */
+
+/**
+ * @typedef {Object} Example
+ */
+
+/**
+ * @typedef {Object} Loc
+ */
+
+/**
+ * @typedef {Object} Members
+ * @property {Comment[]} inner
+ * @property {Comment[]} instance
+ * @property {Comment[]} static
+ */
+
+/**
+ * @typedef {Object} Parameter
+ */
+
+/**
+ * @typedef {Object} Path
+ */
+
+/**
+ * @typedef {Object} Property
+ */
+
+/**
+ * @typedef {Object} Return
+ */
+
+/**
+ * @typedef {Object} Throw
+ */
+
+/**
+ * @typedef {Object} Type
+ */
+
+/**
+ * @typedef {Object} Unist
+ */
+
+/**
+ * @typedef {String} Version
+ */
